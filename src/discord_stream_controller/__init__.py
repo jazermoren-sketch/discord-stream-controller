6cbefae12750b536473371b242d02b5a011424a6
jazermoren-sketch <jazermoren@gmail.com>
2026-08-03 13:22:31 +0100

    Export voice stream controller for v0.2.0

--- src/discord_stream_controller/__init__.py
+++ src/discord_stream_controller/__init__.py
@@ -1,5 +1,6 @@
 from .controller import StreamController
 from .models import MediaSource, StreamStatus
+from .voice import VoiceStreamController
 
-__all__ = ["StreamController", "MediaSource", "StreamStatus"]
-__version__ = "0.1.0"
+__all__ = ["StreamController", "VoiceStreamController", "MediaSource", "StreamStatus"]
+__version__ = "0.2.0"
